2f8ffb817ee96c718ac9a17b3e22ff80ea02d9da
Oohwo <82482899+Oohwo@users.noreply.github.com>
2021-09-02 07:11:13 -0400

    Added test cases

--- exp_taylor.py
+++ exp_taylor.py
@@ -7,3 +7,10 @@ def exp_talyor(x, n):
     for num in range(n):
         estimation = estimation + ((x)**num)/(math.factorial(num))
     return(abs(estimation - e_to_the_x))
+
+# test cases
+print(exp_taylor(3.0, 1)) # 19.085536923187664
+print(exp_taylor(3.0, 5)) # 3.7105369231876644
+print(exp_taylor(3.0, 10)) # 0.022144066044809563
+print(exp_taylor(3.0, 100)) # 3.552713678800501e-15
+print(exp_taylor(4.5, 10)) # 1.5386387816880926
