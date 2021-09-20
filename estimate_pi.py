8ad8381da944481645ec43e38f0521a4d62f378f
Oohwo <82482899+Oohwo@users.noreply.github.com>
2021-09-20 04:02:04 -0400

    Create estimate_pi.py

---
+++ estimate_pi.py
@@ -0,0 +1,43 @@
+# π (Pi) is a magical number. One method to estimate its value(3.141592...) is by using a Monte Carlo method.
+# Assume we have a circle of radius 0.5, enclosed by a 1 × 1 square. The area of the circle is π*r^2=π/4,
+# the area of the square is 1.
+
+# For simplicity, you can imagine a two dimensional coordinate grid, with one vertex of the square being the origin
+# point, (0,0). And a circle with a 0.5 radius centered at (0.5, 0.5).
+
+# We then generate a large number of uniformly distributed random points, throughout the unit square.
+# We keep track of the total number of points M, and the number of points that are inside the unit circle N.
+# If we divide the number of points within the circle, N by the total number of points, M, we should get
+# a value that is an approximation of the ratio of the areas between the circle and square.
+
+#
+#   N       pi * (0.5)^2       pi
+# ----- = -------------- = ----------
+#   M         1 x 1            4
+
+# Simplifying this we get
+
+#
+# 4 N
+# --- = pi
+#  M
+
+# Write a function estimate_pi(M) that using the Monte Carlo method to estimate the value of Pi with total M sampling.
+# Generate a total of M points randomly inside the unit square. Check how many of those points lie within the
+# enclosed circle, N. Return the value of 4*N divided by M as the estimate of π.
+#
+# This being the final lab means we expect your code to NOT crash. That means use default arguments, try catch blocks,
+# do enough error checking so as to handle any call to your function without crashing. In case of a bad argument,
+# your function should return None.
+#
+# Input: 1 input
+#   - M: the number of samples to be generated. Type: int
+#
+# Output: 1 output
+# estimated value of π. Type: float
+#
+# Example:
+# estimate_pi(10) -> 3.2
+# estimate_pi(1000) -> 3.18
+# estimate_pi() -> None
+# estimate_pi('hi') -> None
