5de4a4d181d51705ba0dabb8cfdc093f9f0580f1
Oohwo <82482899+Oohwo@users.noreply.github.com>
2021-09-20 04:03:10 -0400

    Create plot_func.py

---
+++ plot_func.py
@@ -0,0 +1,12 @@
+# Write a function called plot_func() that uses matplotlib.pyplot.plot to produce a plot of the functions
+# f(t) = sin(π/t) when t != 0. If t = 0, f(0) = 0.
+# g(t) = t*exp{-t^2}
+# over the interval [-1.0, 1.0].
+# Include labels for the x- and y-axes, and a legend.
+# * x-label: "t-axes"
+# * y-label: "y-axes"
+# * Legend
+#     * f(t)
+#     * g(t)
+# Make sure to use enough points that the curve is closed and appears smooth.
+# Note: This part will be manually graded!
